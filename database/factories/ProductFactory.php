aa5df6f0b4cd811ec2e062cd8bcb8925830b7235
AhmedArafa2020 <YOU@EXAMPLE.COM>
2019-02-06 18:12:39 +0200

    seeding part of product aand review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,7 +8,7 @@ $factory->define(App\Model\Product::class, function (Faker $faker) {
         'name' =>$faker->word,
         'detail'=>$faker->paragraph,
         'price'=> $faker->numberBetween(100,1000),
-        'img'=>$faker->imageUrl($width = 640, $height = 480), // 'http://lorempixel.com/640/480/'
+       // 'img'=>$faker->image($dir = '/resources/img', $width = 320, $height = 240), 
         'stock'=> $faker->randomDigit,
         'discount'=> $faker->numberBetween(2,30),
     ];
